5e446478f0d3164c68a5921fb28ac8b88179ecf6
zhengj <x614545287@gmail.com>
2018-01-03 18:10:51 +0800

    add aspectj

--- onresultmanager/src/main/java/io/github/anotherjack/onresultmanager/OnResultAspect.java
+++ onresultmanager/src/main/java/io/github/anotherjack/onresultmanager/OnResultAspect.java
@@ -3,9 +3,12 @@ package io.github.anotherjack.onresultmanager;
 import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 /**
  * Created by zhengj on 2018/1/3.
@@ -22,8 +25,21 @@ public class OnResultAspect {
         int requestCode = (int) args[0];
         int resultCode = (int) args[1];
         Intent data = (Intent) args[2];
-        Activity activity = (Activity) joinPoint.getTarget(); //这里我也试了使用joinPoint.getThis()，发现也能触发，这里还不太清楚
+        Activity activity = (Activity) joinPoint.getTarget(); //这里我也试了使用joinPoint.getThis()，发现也能触发，
+                                                                // 打断点发现两者好像是同一个对象，这里还不太清楚
+
         new OnResultManager(activity).trigger(requestCode,resultCode,data);
 
+    }
+
+    @Before("execution(* android.widget.Toast.show())")
+    public void showToastBefore(JoinPoint joinPoint) throws Throwable {
+        Log.d(TAG,"-------------Toast#show() hooked");
+    }
+
+    @Before("execution(* android.app.Activity.on**(..))")
+    public void onActivityMethodBefore(JoinPoint joinPoint) throws Throwable {
+        String key = joinPoint.getSignature().toString();
+        Log.d(TAG, "onActivityMethodBefore: " + key);
     }
 }
